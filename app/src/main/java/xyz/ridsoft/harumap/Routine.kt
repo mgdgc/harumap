eeafc83d27912cc322d5262999b249f42fe37baf
soc06212 <soc06212@gmail.com>
2021-11-10 14:22:41 +0900

    Feat(Routine): 객체 구조 변경 및 SQL CREATE문 추가

--- app/src/main/java/xyz/ridsoft/harumap/Routine.kt
+++ app/src/main/java/xyz/ridsoft/harumap/Routine.kt
@@ -1,6 +1,15 @@
 package xyz.ridsoft.harumap
 
 data class Routine(val id: Int, var content: String) {
+    companion object {
+        const val SQL_CREATE = "CREATE TABLE IF NOT EXISTS ROUTINE (" +
+                "id INTEGER PRIMARY KEY AUTO_INCREMENT," +
+                "content TEXT," +
+                "enabled INTEGER DEFAULT 1," +
+                "notification INTEGER DEFAULT 0" +
+                ");"
+    }
+
     var enabled: Boolean = true
     var notification: Boolean = true
 
@@ -23,6 +32,42 @@ data class Routine(val id: Int, var content: String) {
     }
 }
 
-data class Task(val _id: Int, var id: Int, var week: Int, var dayOfWeek: Int, var complete: Boolean)
+data class Task(
+    val _id: Int,
+    var year: Int,
+    var week: Int,
+    var done: Array<Int>
+) {
+    companion object {
+        const val SQL_CREATE = "CREATE TABLE IF NOT EXISTS TASK (" +
+                "_id INTEGER PRIMARY KEY AUTO_INCREMENT," +
+                "year INTEGER," +
+                "week INTEGER," +
+                "done TEXT DEFAULT \"{0, 0, 0, 0, 0, 0, 0}\"," +
+                "total TEXT DEFAULT \"{0, 0, 0, 0, 0, 0, 0}\"," +
+                ");"
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Task
+
+        if (_id != other._id) return false
+        if (year != other.year) return false
+        if (week != other.week) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = _id
+        result = 31 * result + year
+        result = 31 * result + week
+        return result
+    }
+
+}
 
 class Week(var week: Int, var done: Array<Int>, var tasks: Array<Int>)
