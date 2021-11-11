225cced325fcc0db3558e9ae59210562163605d1
soc06212 <soc06212@gmail.com>
2021-11-11 21:32:30 +0900

    Feat(DataManager): DataManager가 초기화될 때 Task와 Routine을 초기화하는 코드 추가

--- app/src/main/java/xyz/ridsoft/harumap/DataManager.kt
+++ app/src/main/java/xyz/ridsoft/harumap/DataManager.kt
@@ -1,16 +1,46 @@
 package xyz.ridsoft.harumap
 
 import android.content.Context
+import java.util.*
 
 class DataManager(private val context: Context) {
     companion object {
         private var initialized = false
+        var routines: MutableMap<Int, Routine> = mutableMapOf()
         var tasks: MutableMap<Int, Task> = mutableMapOf()
     }
 
+    private val dbHelper = DBHelper(context)
+
     init {
         if (!initialized) {
+            // Initialize routines
+            initRoutines()
 
+            // Initialize tasks
+            initTasks()
+        }
+    }
+
+    fun initRoutines() {
+        // Clear data
+        DataManager.routines.clear()
+
+        // Get data from db
+        val routines = dbHelper.getRoutines()
+        for (routine in routines) {
+            DataManager.routines[routine.id] = routine
+        }
+    }
+
+    fun initTasks() {
+        // Clear data
+        DataManager.tasks.clear()
+
+        // Get data from db
+        val tasks = dbHelper.getTasks("SELECT * FROM Task ORDER BY ASC")
+        for (task in tasks) {
+            DataManager.tasks[task._id] = task
         }
     }
 }
