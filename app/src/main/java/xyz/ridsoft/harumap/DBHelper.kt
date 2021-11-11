94df326acfd2b4596b1b28438d8c3922d0c8f0b4
soc06212 <soc06212@gmail.com>
2021-11-11 21:31:28 +0900

    Feat(DBHelper): DBHelper 기능 추가

--- app/src/main/java/xyz/ridsoft/harumap/DBHelper.kt
+++ app/src/main/java/xyz/ridsoft/harumap/DBHelper.kt
@@ -4,6 +4,9 @@ import android.content.Context
 import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.Gson
+import java.util.*
+import kotlin.collections.ArrayList
 
 class DBHelper(private val context: Context) {
 
@@ -17,17 +20,55 @@ class DBHelper(private val context: Context) {
             db = context.openOrCreateDatabase(
                 DB.DB, AppCompatActivity.MODE_PRIVATE, null
             )
+
+            db.execSQL(Routine.SQL_CREATE)
+            db.execSQL(Task.SQL_CREATE)
         }
     }
 
+    fun getRoutines(): ArrayList<Routine> {
+        return getRoutines("SELECT * FROM Routine")
+    }
+
+    fun getRoutines(sql: String): ArrayList<Routine> {
+        val cursor = db.rawQuery(sql, null)
+        return toRoutines(cursor)
+    }
+
+    fun toRoutines(cursor: Cursor): ArrayList<Routine> {
+        val routines: ArrayList<Routine> = arrayListOf()
+        while (cursor.moveToNext()) {
+            // Create routine object
+            val routine = Routine(
+                cursor.getInt(cursor.getColumnIndex("id")),
+                cursor.getString(cursor.getColumnIndex("content"))
+            )
+            routine.enabled = cursor.getInt(cursor.getColumnIndex("enabled")) == 1
+            routine.notification = cursor.getInt(cursor.getColumnIndex("notification")) == 1
+
+            routines.add(routine)
+        }
+
+        return routines
+    }
+
     fun getTasks(sql: String): ArrayList<Task> {
+        val cal = Calendar.getInstance()
+        val year = cal.get(Calendar.YEAR)
+        val week = cal.get(Calendar.WEEK_OF_YEAR)
+        val json = "\"{0, 0, 0, 0, 0, 0, 0}\""
+        db.execSQL("INSERT OR IGNORE INTO TASK (year, week, done, total) VALUES($year, $week, $json, $json)")
         val cursor = db.rawQuery(sql, null)
         return toTasks(cursor)
     }
 
-    fun getTasks(week: Int): ArrayList<Task> {
-        val sql = "SELECT * FROM Task WHERE week = $week;"
-        return getTasks(sql)
+    fun getTask(year: Int, week: Int): Task {
+        val sql =
+            "SELECT * FROM Task WHERE year = $year AND week = $week;"
+
+        val tasks = getTasks(sql)
+
+        return tasks[0]
     }
 
     private fun toTasks(cursor: Cursor): ArrayList<Task> {
@@ -36,10 +77,12 @@ class DBHelper(private val context: Context) {
             tasks.add(
                 Task(
                     cursor.getInt(cursor.getColumnIndex("_id")),
-                    cursor.getInt(cursor.getColumnIndex("id")),
+                    cursor.getInt(cursor.getColumnIndex("year")),
                     cursor.getInt(cursor.getColumnIndex("week")),
-                    cursor.getInt(cursor.getColumnIndex("dayOfWeek")),
-                    cursor.getInt(cursor.getColumnIndex("complete")) == 1
+                    Gson().fromJson(
+                        cursor.getString(cursor.getColumnIndex("dayOfWeek")),
+                        Array<Int>::class.java
+                    )
                 )
             )
         }
@@ -49,7 +92,12 @@ class DBHelper(private val context: Context) {
     }
 
     fun update(task: Task) {
-        val sql = "UPDATE Task SET complete = ${task.complete} WHERE id = ${task._id};"
+        val sql =
+            "INSERT OR REPLACE INTO Task (year, week, done, total) VALUES (" +
+                    "${task.year}, " +
+                    "${task.week}, " +
+                    Gson().toJson(task.done) +
+                    ") WHERE _id = ${task._id};"
         db.execSQL(sql)
     }
 
