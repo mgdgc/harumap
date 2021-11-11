f870d6a11d3e94d8e01b23c1f0d9065650dfe7a1
soc06212 <soc06212@gmail.com>
2021-11-11 21:33:11 +0900

    Feat(SharedPreferenceKeys): SharedPreference에 사용하는 Key들을 모아놓은 클래스 추가

---
+++ app/src/main/java/xyz/ridsoft/harumap/SharedPreferenceKeys.kt
@@ -0,0 +1,10 @@
+package xyz.ridsoft.harumap
+
+class SharedPreferenceKeys {
+    companion object {
+        const val KEY_ROUTINES = "routines"
+        const val INT_YEAR = "year"
+        const val INT_DAY_OF_YEAR = "dayOfYear"
+
+    }
+}
