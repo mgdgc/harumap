959f8c5cc7bab454dd6b6b9234ac42b5d15be566
soc06212 <soc06212@gmail.com>
2021-11-16 12:53:49 +0900

    Refactor(RoutineFragment): 항목이 변경되면 DB에 업데이트하는 코드 추가

--- app/src/main/java/xyz/ridsoft/harumap/RoutineFragment.kt
+++ app/src/main/java/xyz/ridsoft/harumap/RoutineFragment.kt
@@ -8,11 +8,15 @@ import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import xyz.ridsoft.harumap.databinding.FragmentRoutineBinding
+import java.util.*
 
 class RoutineFragment : Fragment() {
 
     private lateinit var binding: FragmentRoutineBinding
     private lateinit var adapter: RoutineAdapter
+    private lateinit var dbHelper: DBHelper
+    private lateinit var task: Task
+    private var dayOfWeek: Int = 0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,13 +30,37 @@ class RoutineFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        initTask()
+        initRecyclerView()
+    }
+
+    private fun initTask() {
+        // Get calendar
+        val cal = Calendar.getInstance()
+        val year = cal.get(Calendar.YEAR)
+        val week = cal.get(Calendar.WEEK_OF_YEAR)
+        dayOfWeek = cal.get(Calendar.DAY_OF_WEEK)
+
+        dbHelper = DBHelper(requireContext())
+        task = dbHelper.getTask(year, week)
+    }
+
+    private fun initRecyclerView() {
         // Initialize recyclerview
         adapter = RoutineAdapter(requireContext())
         adapter.onStateChangedListener = { id, complete ->
-            DataManager.tasks[id]?.complete = complete
-            // update db
-            val dbHelper = DBHelper(requireContext())
-//            dbHelper.update()
+            // Update preference
+            val routinePref =
+                requireContext().getSharedPreferences(SharedPreferenceKeys.KEY_ROUTINES, 0)
+            val edit = routinePref.edit()
+            edit.putBoolean(id.toString(), complete)
+            edit.apply()
+
+            // Update db
+            task.routines[id] = complete
+
+            dbHelper.update(task)
+
         }
 
         binding.rvRoutine.layoutManager = LinearLayoutManager(requireContext())
