bc3e7335df9091985b2fe2223d14c4738067a305
soc06212 <soc06212@gmail.com>
2021-11-19 11:50:58 +0900

    Feat(Settings): 설정 화면 추가

---
+++ app/src/main/java/xyz/ridsoft/harumap/ui/settings/SettingsActivity.kt
@@ -0,0 +1,12 @@
+package xyz.ridsoft.harumap.ui.settings
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import xyz.ridsoft.harumap.R
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
